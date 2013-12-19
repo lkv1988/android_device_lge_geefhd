be4029c6e5b4d39fbfd58004827a3563c78fcc2c
Kevin Liu <airk908@gmail.com>
2013-12-19 14:26:11 +0800

    Try to fix some display bugs, and add a err log for later fixing
    Signed-off-by: Kevin Liu <airk908@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,7 +6,7 @@
 -include device/lge/geefhd-common/BoardConfigCommon.mk
 
 
-BOARD_KERNEL_CMDLINE := vmalloc=600M console=ttyHSL0,115200,n8 lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=geefhd uart_console=disable lge.rev=rev_10 lcd_maker_id=primary lge.hreset=off, lge.reset=rst_etc gpt=enable logoentry=0x77665501+0+0 lge.batt_info=ds2704_l lge.crash=0x0 lge.crash_enable=0x0 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := vmalloc=600M console=ttyHSL0,115200,n8 lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=geefhd uart_console=disable lge.rev=rev_10 lcd_maker_id=primary lge.hreset=off, lge.reset=rst_etc gpt=enable logoentry=0x77665501+0+0 lge.batt_info=ds2704_l lge.crash=0x0 lge.crash_enable=0x0
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 
@@ -21,6 +21,13 @@ RECOVERY_FSTAB_VERSION = 2
 # assert
 TARGET_OTA_ASSERT_DEVICE := geefhd,geefhd_hk,geefhd_open_hk,gkatt
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/geefhd/loki
+
+# OpenGL
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+#USE_DEVICE_SPECIFIC_CAMERA := true
+#USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY := true
+TARGET_DISPLAY_INSECURE_MM_HEAP := true
 
 # inherit from the proprietary version
 -include vendor/lge/geefhd/BoardConfigVendor.mk
